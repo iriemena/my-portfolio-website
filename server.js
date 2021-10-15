8f8eb925eb4b01cdc0203bbd2d54477c06924e6a
iriemena <fortunateweb@yahoo.com>
2021-10-15 20:51:48 +0100

    re

--- server.js
+++ server.js
@@ -8,18 +8,18 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 4000;
 
-app.use(express.static(path.join(__dirname, "../client/build")));
+// app.use(express.static(path.resolve(__dirname, "../client/build")));
 // This middleware informs the express application to serve our compiled React files
-// if (
-//   process.env.NODE_ENV === "production" ||
-//   process.env.NODE_ENV === "staging"
-// ) {
-//   app.use(express.static(path.join(__dirname, "client/build")));
+if (
+  process.env.NODE_ENV === "production" ||
+  process.env.NODE_ENV === "staging"
+) {
+  app.use(express.static(path.join(__dirname, "client/build")));
 
-//   app.get("*", function (req, res) {
-//     res.sendFile(path.join(__dirname, "client/build", "index.html"));
-//   });
-// }
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 
 app.use(express.json());
 app.use(cors());
